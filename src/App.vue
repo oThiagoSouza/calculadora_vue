1c55b450453903c6f3b60976e4ee15780fff3c98
oThiagoSouza <souzaalvesthiago99@gmail.com>
2023-05-12 15:25:20 -0300

    Finalizando calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,105 @@
 <script setup>
+import { reactive } from 'vue';
 
+
+const calculadora = reactive({
+  resultado: 0,
+  num1: 0,
+  num2: 0,
+  operation: 'sum',
+});
+
+function calculadora1(evt) {
+  
+  calculadora.num1 = parseInt(evt);
+
+  aritimetic(calculadora.operation);
+}
+
+
+function calculadora2(evt) {
+
+  calculadora.num2 = parseInt(evt);
+
+  aritimetic(calculadora.operation);
+
+}
+
+function aritimetic(evt) {
+  calculadora.operation = evt;
+
+  if (calculadora.operation == 'sum') {
+    sum(calculadora.num1, calculadora.num2);
+  } else if (calculadora.operation == 'substract') {
+    substract(calculadora.num1, calculadora.num2);
+  } else if (calculadora.operation == 'multiply') {
+    multiply(calculadora.num1, calculadora.num2);
+  } else if (calculadora.operation == 'divide') {
+    divide(calculadora.num1, calculadora.num2);
+  }
+
+}
+function sum(num1, num2) {
+  calculadora.resultado = num1 + num2;
+}
+
+function substract(num1, num2) {
+  calculadora.resultado = num1 - num2;
+}
+
+function multiply(num1, num2) {
+  calculadora.resultado = num1 * num2;
+}
+
+function divide(num1, num2) {
+  calculadora.resultado = num1 / num2;
+}
 </script>
 
 <template>
-  <div class="container">
-    <header class="pb-3 pt-3 mb-5 mt-5 bg-light rounded-4" >
-      <h1 class="text-center">
-        Calculadora com VueJS
-      </h1>
-    </header>
-    <div class="row">
-      <div class="col d-flex">
-        <input type="number" class="num1 form-control me-2">
-        <input type="number" class="num2 form-control me-2 ">
-        <button type="button" class="me-2 btn btn-primary">Calcular</button>
-        <select id="operacao" class="text-center form-control">
-          <option value="Adição">Adição</option>
-          <option value="Adição">Subtração</option>
-          <option value="Adição">Multiplicação</option>
-          <option value="Adição">Divisão</option>
-        </select>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <span>
-            
-        </span>
-      </div>
-    </div>
+  <div id="calculadora">
+    <h1>Calculadora</h1>
+    <input type="number" id="number1" name="number1" @keyup="calculadora1($event.target.value)">
+
+    <select @change="aritimetic($event.target.value)">
+      <option selected value="sum">+</option>
+      <option value="substract">-</option>
+      <option value="multiply">*</option>
+      <option value="divide">/</option>
+    </select>
+
+    <input type="number" id="number2" name="number2" @keyup="calculadora2($event.target.value)">
+
+    <h3>= {{ calculadora.resultado }}</h3>
+   
   </div>
 </template>
 
 <style scoped>
-  .num1,.num2{
-    max-width: 150px;
+  * {
+    font-family: sans-serif;
   }
-  .btn {
-    max-width: 150px;
-    width: 100%;
+
+  h1 {
+    display: flex;
+    justify-content: center;
   }
-  
-  #operacao {
-    width: 150px;
+  #calculadora {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px;
+  }
+
+  input, select {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 350px;
+  }
+
+  h3 {
+    margin-top: 30px;
   }
 </style>
